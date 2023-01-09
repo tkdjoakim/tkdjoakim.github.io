e24cd95586c01cb45f71715154e66b7cfb9ccd49
tkdjoakim <116543930+tkdjoakim@users.noreply.github.com>
2023-01-09 21:03:22 +0100

    Update script.js

--- Vokalfjerner/JS/script.js
+++ Vokalfjerner/JS/script.js
@@ -1,1 +1,18 @@
 
+
+
+
+
+
+
+function vokal(bokstaver){
+let vokal = a, e, i, o, y
+
+for(let i = bokstaver; i < tall.length; i++) {
+vokal + bokstaver[i]
+
+
+}
+console.log[i]
+
+return [bokstaver]
