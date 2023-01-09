3567eb1d2019db0c7d04f4bab4643fe6bc229621
tkdjoakim <116543930+tkdjoakim@users.noreply.github.com>
2023-01-09 21:08:55 +0100

    Add files via upload

---
+++ Vokalfjerner/JS/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+function vokal(bokstaver){
+let vokal = a, e, i, o, y
+
+for(let i = bokstaver; i < tall.length; i++) {
+vokal + bokstaver[i]
+
+
+}
+console.log[i]
+
+return [bokstaver]
+}
